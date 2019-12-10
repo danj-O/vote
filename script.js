d5a187b2daea74d52fe694749a2555891c88b8be
danj-O <danjowanjo@gmail.com>
2019-12-10 15:18:24 -0500

    first scene forms finished

--- script.js
+++ script.js
@@ -1,25 +1,50 @@
-const titleBox = document.getElementById("titlebox"); //top box for title
+const titleBox = document.getElementById("titlebox"); //title box
 const title = document.getElementById("title");
 
-const answerBox = document.getElementById("answerbox"); //bottom box for answer stuff
-const howMany = document.getElementById("howmany");
+const peopleBox = document.getElementById("peoplebox"); //ppl box
+const people = document.getElementById("people");
 
+const voteBox = document.getElementById("votebox"); // box for voting
+const choiceInput = document.getElementById("choiceinput");
+
+const continueBtn = document.getElementById("continueBtn");
 
 const enter = () => {
     const x = title.value;
     titleBox.innerHTML = x;
-    const q = howMany.value;
-    answerBox.removeChild(howMany);
-    answerBox.removeChild(continueBtn);
+
+    const q = people.value;  // q is how many ppl playing
+    peopleBox.innerHTML = q + " People";
     console.log(q);
-    for (let i = 0; i < q; i++){
-        console.log("yep");
-        const newInput = document.createElement("input");
-        answerBox.appendChild(newInput);
-    }
+
+    const y = choiceInput.value;
+    voteBox.innerHTML = y;
+
+    continueBtn.remove();
 
 }
 
+
+
+
+const enter2 = () => {
+
+}
+const addChoice = () => {
+    const newChoice = document.createElement("input");
+
+    voteBox.appendChild(newChoice);
+
+}
+
+
+
+// for (let i = 0; i < q; i++){
+//     console.log("yep");
+//     const newInput = document.createElement("input");
+//     newInput.placeholder = "write a choice here";
+//     answerBox.appendChild(newInput);
+// }
 
 // document.createElement("answerbox").innerHTML = `
 // <input type="text" class="vote_choices" placeholder="option">`;
