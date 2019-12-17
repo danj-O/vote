1586ccaa92386da09c2b8bfa9e2309679755554a
danj-O <danjowanjo@gmail.com>
2019-12-17 15:41:34 -0500

    the math works!

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const titleBox = document.getElementById("titlebox"); //title box
 const title = document.getElementById("title");
+const inputStuff = document.getElementById("inputstuff");
 
 const peopleBox = document.getElementById("peoplebox"); //ppl box
 const people = document.getElementById("people");
@@ -55,6 +56,7 @@ const enter = () => {
         const inputVal = document.getElementById(`choiceinput${i}`);
         // choiceButton.data-value = 
         choiceButton.innerHTML = inputVal.value;
+        choiceButton.className = 'choicebtn';
         choiceButton.value = inputVal.value;
         choiceButton.id = `choicebutton`+ i;
         choiceButton.onclick = voteClick;
@@ -93,11 +95,11 @@ const voteClick = (e) => {
         if (peopleCount == 0){
             votesArr.push(firstOption, secondOption, thirdOption, fourthOption, fifthOption);
             votesLeft.innerHTML = `<button id="resultsBtn" onclick= "findNumInArr()">Click here to see results!</button>`;
-            console.log('votes ar', votesArr);
+            // console.log('votes ar', votesArr);
         } else if (peopleCount < 0){
             peopleCount = null;
         }          
-        console.log('people count', peopleCount);
+        // console.log('people count', peopleCount);
         // alert("NEXT! PASS THE PHONE!");  //SEND ALERT B/W VOTERS TURNS
     }
 }
@@ -108,31 +110,47 @@ const voteClick = (e) => {
 const findNumInArr = () => {
     const votesArrLen = votesArr.length;
     const votesArrCopy = votesArr.slice(0);
-    console.log('before',votesArr);
+    // console.log('before',votesArr);
     const sortedVotesArr = votesArr.sort();
-    const winVoteAmt = sortedVotesArr.pop();
-    const winVoteAmt2 = sortedVotesArr.pop();
+    const winVoteAmt = sortedVotesArr.slice(-1);
+    sortedVotesArr.pop();
+    // console.log('sorted array before', sortedVotesArr);
+    const winVoteAmt2 = sortedVotesArr.slice(-1);
+    // sortedVotesArr.pop();
+
     // const winVoteAmt = votesArr.slice(-1);
-    console.log('yep');
-    console.log('copy',votesArrCopy);
-    console.log('length',votesArrLen);
-    console.log('sorted array', sortedVotesArr);
+    // console.log('votes arr', votesArr);
+    // console.log('copy',votesArrCopy);
+    // console.log('length',votesArrLen);
+    // console.log('sorted array', sortedVotesArr);
     console.log('winning vote has', winVoteAmt);
-    for (i=1; i < votesArrLen;i++){
-        if (votesArrCopy[i] == winVoteAmt){
-            const woo = document.getElementById(`choicebutton${i+1}`);
-            console.log(woo);
-            const winningOption = woo.value;
-            if (votesArrCopy[i] !== winVoteAmt2){
+    console.log('winning vote 2 has', winVoteAmt2);
+
+    // debugger;
+    for (i=0; i < votesArrLen;i++){
+        console.log('i', i);
+        if (votesArrCopy[i] == winVoteAmt[0]){
+            console.log(votesArrCopy[i]);
+            if (votesArrCopy[i] !== winVoteAmt2[0]){
+                const woo = document.getElementById(`choicebutton${i+1}`);
+                console.log(woo);
+                const winningOption = woo.value;
                 console.log('win op 1',winningOption);
                 peopleBox.innerHTML = `The winner is ${winningOption}`;
-                votesLeft.remove();
-            } else if (votesArrCopy[i] == winVoteAmt2){
-            const woo2 = document.getElementById(`choicebutton${i+1}`);
-            const winningOption2 = woo2.value;
-            console.log('win op 2',winningOption2);
-            peopleBox.innerHTML = `We have a tie!`;
+                // votesLeft.remove();
+                // votesArrCopy.pop();
+            } else if (votesArrCopy[i] == winVoteAmt2[0]){
+                const woo2 = document.getElementById(`choicebutton${i+1}`);
+                const winningOption2 = woo2.value;
+                console.log('win op 2',winningOption2);
+                // votesLeft.innerHTML = `${winningOption2}`;
+                peopleBox.innerHTML = `We have a tie!`;
+                const winner = document.createElement('div');
+                winner.innerHTML = `${winningOption2}`;
+                inputStuff.appendChild(winner);
             }
-        }
-    }
-}
+        } 
+    }votesLeft.remove();
+    voteBox.remove();
+} 
+
